3c87095ae95a00ed65483391524d49881419a5a5
Bernard Mordan <bernardmordan@gmail.com>
2014-07-05 14:22:04 +0100

    initial touch array experiment

--- fingertracker.js
+++ fingertracker.js
@@ -8,17 +8,24 @@ if (Meteor.isClient) {
     var delta = $('#delta');
     var el = this.find('#pad');
     Hammer(el).on('touch', function(e){
+      e.gesture.preventDefault();
       console.log(e);
-      delta[0].innerHTML = 'X=' + e.gesture.srcEvent.x + ' Y=' + e.gesture.srcEvent.y;
-      $('#marker').css('left',e.gesture.srcEvent.x-10);
-      $('#marker').css('top',e.gesture.srcEvent.y-10);
+      var touches = e.gesture.touches.length;
+      e.target.children.delta.innerHTML = 'Single touch/click';
+      e.target.children.markerO.style.left = e.gesture.touches[0].pageX-10 + 'px';
+      e.target.children.markerO.style.top = e.gesture.touches[0].pageY-10 + 'px';      
+      if(touches >= 2){
+        e.target.children.delta.innerHTML = 'Multi touch ' + touches + ' | ';
+        e.target.children.markerD.style.left = e.gesture.touches[1].pageX-10 + 'px';
+        e.target.children.markerD.style.top = e.gesture.touches[1].pageY-10 + 'px';
+      }      
     });
-  };
-  
-}
+  }; 
+}//meteor is no longer client
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    // sudo rm -rf theInternet <<-- the most dangerous code in the world?
   });
 }
