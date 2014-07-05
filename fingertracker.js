46e01af82c16b62fafe48e9554e3cc55cad3d886
Bernard Mordan <bernardmordan@gmail.com>
2014-07-05 17:11:03 +0100

    complete two touches

--- fingertracker.js
+++ fingertracker.js
@@ -7,19 +7,23 @@ if (Meteor.isClient) {
   Template.pad.rendered = function(template){
     var delta = $('#delta');
     var el = this.find('#pad');
-    Hammer(el).on('touch', function(e){
+    
+    Hammer(el).on('touch drag transform', function(e){
       e.gesture.preventDefault();
       console.log(e);
       var touches = e.gesture.touches.length;
-      e.target.children.delta.innerHTML = 'Single touch/click';
+      //Single-touch
       e.target.children.markerO.style.left = e.gesture.touches[0].pageX-10 + 'px';
-      e.target.children.markerO.style.top = e.gesture.touches[0].pageY-10 + 'px';      
+      e.target.children.markerO.style.top = e.gesture.touches[0].pageY-10 + 'px';
+      e.target.children.delta.innerHTML = '1 touch ' + String(e.gesture.touches[0].pageX-10) + ',' +  String(e.gesture.touches[0].pageY-10);  
+      //multi-touch
       if(touches >= 2){
-        e.target.children.delta.innerHTML = 'Multi touch ' + touches + ' | ';
         e.target.children.markerD.style.left = e.gesture.touches[1].pageX-10 + 'px';
         e.target.children.markerD.style.top = e.gesture.touches[1].pageY-10 + 'px';
-      }      
+        e.target.children.delta.innerHTML = '2 touches ' + String(e.gesture.touches[0].pageX-10) + ',' +     String(e.gesture.touches[0].pageY-10) + ' | ' + String(e.gesture.touches[1].pageX-10) + ',' +     String(e.gesture.touches[1].pageY-10);
+      }
     });
+    
   }; 
 }//meteor is no longer client
 
